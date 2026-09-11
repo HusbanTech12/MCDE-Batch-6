7abbc1fe910522b6adfe730d6533cc8b680436a3
HusbanTech12 <husbantech08@gmail.com>
2026-09-11 18:46:07 +0500

    Assignment 4 - Advacne JOINS

---
+++ Assignment 4 - Advacne JOINS.sql
@@ -0,0 +1,19 @@
+
+-- Task 41: List each staff member alongside their manager's full name.
+-- If a staff member has no manager (top-level), still show them with NULL for manager name.
+
+SELECT * FROM sales.staffs;
+
+
+
+SELECT 
+ e.first_name + ' ' + e.last_name AS employee,
+ m.first_name + ' ' + m.last_name AS manager
+FROM sales.staffs AS e
+LEFT JOIN sales.staffs AS m
+ON e.manager_id = m.staff_id;
+
+
+-- Task 42: Find pairs of products from the same brand that have the exact same list price.
+-- Show both product names and the brand name.
+
